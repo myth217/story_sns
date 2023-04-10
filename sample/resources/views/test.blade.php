23516c57016fd86eaff165424d4df35b0cf27331
myth217 <sn0217120@gmail.com>
2023-04-10 18:41:25 +0900

    環境初期 mock 削除

---
+++ sample/resources/views/test.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="./test/css/common.css">
+  <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script> 
+  <script type='text/javascript'  src='./mock/js/tablet.js'></script>
+  <title>SNS</title>
+</head>
+<body>
+  <header>
+    <h1>Welcome to SNS!</h1>
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Profile</a></li>
+        <li><a href="#">Messages</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <section>
+      <h2>Recent Posts</h2>
+      <ul>
+        <li>
+          <h3>Post Title</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et quam sapien. Vivamus vel tortor leo. </p>
+          <time datetime="2023-04-05T12:34:56">5 minutes ago</time>
+        </li>
+        <li>
+          <h3>Another Post Title</h3>
+          <p>Nulla venenatis orci at erat bibendum, sed hendrerit tellus elementum.</p>
+          <time datetime="2023-04-05T12:00:00">30 minutes ago</time>
+        </li>
+      </ul>
+    </section>
+    <section>
+      <h2>Popular Tags</h2>
+      <ul>
+        <li><a href="#">#tag1</a></li>
+        <li><a href="#">#tag2</a></li>
+        <li><a href="#">#tag3</a></li>
+      </ul>
+    </section>
+  </main>
+  <footer>
+    <p>&copy; 2023 SNS</p>
+  </footer>
+
+
+</body>
+</html>
