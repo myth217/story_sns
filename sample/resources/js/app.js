abcc0e4502fde3505117c351c707e7f246687698
myth217 <sn0217120@gmail.com>
2023-04-13 18:06:17 +0900

    ログイン時のみのルート作成

--- sample/resources/js/app.js
+++ sample/resources/js/app.js
@@ -1,6 +1,1 @@
 import './bootstrap';
-
-import "@fortawesome/fontawesome-free/js/fontawesome";
-import "@fortawesome/fontawesome-free/js/brands";
-import "@fortawesome/fontawesome-free/js/solid";
-import "@fortawesome/fontawesome-free/js/regular";
--- sample/routes/web.php
+++ sample/routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Models\User as ModelsUser;
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Support\Facades\Route;
 
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
+
+
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,3 +30,9 @@ Route::get('test', function () {
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+// ログインしている場合のみ以下のルートを許可する
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/users', [UserController::class, 'index']) -> name('users.index');
+});
