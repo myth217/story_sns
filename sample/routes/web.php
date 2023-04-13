ff6fa51476c2c3d7e5cecd75ef0f80d2ff89796e
myth217 <sn0217120@gmail.com>
2023-04-13 17:40:12 +0900

    sns 基礎構成 構築

--- sample/routes/web.php
+++ sample/routes/web.php
@@ -17,13 +17,11 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 Route::get('test', function () {
     return view('test');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
